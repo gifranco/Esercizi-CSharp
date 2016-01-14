3f5b20923e98aa82132e23968f7d22dd4cb30c41
ingifranco <giovanni.franco12@tin.it>
2016-01-14 22:28:43 +0100

    14/01/2016 - Aggiornamento

--- Esercizi-CSharp/Class1.cs
+++ Esercizi-CSharp/Class1.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Classe di tools per la soluzione "Esercizi-CSharp"
+/// </summary>
 namespace Esercizi_CSharp
 {
     public class Utility
     {
         static public void Ferma()
         {
-            Console.Write("\nPremi un tasto per continuare ...");
-            Console.ReadKey();
-        }
-
-        static public void Alt()
-        {
-            Console.Clear();
             Console.Write("\nPremi un tasto per continuare ...");
             Console.ReadKey();
         }
--- Esercizio 9/Program.cs
+++ Esercizio 9/Program.cs
@@ -13,6 +13,31 @@ namespace Esercizio_9
         {
             Utility.Inizia();
 
+            Console.WriteLine("MIN(byte) = {0}", System.Byte.MinValue);
+            Console.WriteLine("MAX(byte) = {0}", System.Byte.MaxValue);
+
+            Console.WriteLine("MIN(Decimal) = {0}", System.Decimal.MinValue);
+            Console.WriteLine("MAX(Decimal) = {0}", System.Decimal.MaxValue);
+            Console.WriteLine("MIN(long) = {0}", System.Int64.MinValue);
+            Console.WriteLine("MAX(long) = {0}", System.Int64.MaxValue);
+
+            // boxing
+            long j = 122345L;
+            Console.WriteLine(j);
+            object obj = j;
+
+            Console.WriteLine(obj);
+            // unboxing
+            long k = (long)j;
+            Console.WriteLine(k);
+
+            var a = "ciao";
+            var b = 200UL;
+            var c = 1.2345F;
+            Console.WriteLine("tipo a: {0}", a.GetType());
+            Console.WriteLine("tipo b: {0}", b.GetType());
+            Console.WriteLine("tipo c: {0}", c.GetType());
+
             Utility.Ferma();
         }
     }
