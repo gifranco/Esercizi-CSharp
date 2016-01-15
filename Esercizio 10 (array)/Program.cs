426a003facbc2a30b253e7a3fe50d7842991f89f
ingifranco <giovanni.franco12@tin.it>
2016-01-15 16:14:10 +0100

    Rinominati gli esercizi

---
+++ Esercizio 10 (array)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esercizi_CSharp;
+
+namespace Esercizio_10__array_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility.Inizia();
+
+            /// jagged array
+            int[][] vet = new int[3][];
+
+            vet[1] = new int[1];
+            vet[2] = new int[2];
+            vet[3] = new int[3];
+
+
+
+            Utility.Ferma();
+        }
+    }
+}
--- Esercizio 7/Program.cs
+++ Esercizio 7/Program.cs
@@ -34,6 +34,35 @@ namespace Esercizio_7
                 Console.WriteLine("True");
             }
             Utility.Ferma();
+
+            Utility.Inizia();
+
+            int? num = null;
+
+            // Is the HasValue property true?
+            if (num.HasValue)
+            {
+                System.Console.WriteLine("num = " + num.Value);
+            }
+            else
+            {
+                System.Console.WriteLine("num = Null");
+            }
+
+            // y is set to zero
+            int y = num.GetValueOrDefault();
+
+            // num.Value throws an InvalidOperationException if num.HasValue is false
+            try
+            {
+                y = num.Value;
+            }
+            catch (System.InvalidOperationException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+            Utility.Ferma();
+
         }
     }
 }
